ce67815cecbc82038476f912b51089dd7e512c88
Sean Chen <brand.identity.brand@gmail.com>
2023-05-03 15:39:03 -0400

    TODO: check Window && WindowFrame.

---
+++ src/lib/DesktopEnvironmentContextProvider.jsx
@@ -0,0 +1,55 @@
+import { createContext, useState, useEffect, useRef } from "react";
+import { htmlToElement } from "./utils";
+
+export const DesktopEnvironmentContext = createContext(null);
+
+/**
+ * <DesktopEnviornmentProvider>
+ *     <WindowManagerContextProvider>
+ *          <App/>
+ *     </WindowManagerContextProvider>
+ * </DesktopEnviornmentProvider>
+ */
+
+export default function DesktopEnvironmentContextProvider({children}){
+    // create and empty img element for drag img
+    useEffect(()=>{
+        /* append an empty image to body if it doesnt exist */
+        if ( !document.getElementById('dragImg') ){
+            const img = htmlToElement( /* HTML */ `
+                <img 
+                    id='dragImg' 
+                    alt='' 
+                    src='data:image/png;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
+                />
+            `)
+            document.body.append(img);
+        }
+        // * https://stackoverflow.com/questions/524696/how-to-create-a-style-tag-with-javascript
+        const css = `
+            .rde-display-none { 
+                display: none; 
+            }
+            .rde-unselectable {
+                -webkit-touch-callout: none;
+                -webkit-user-select: none;
+                -khtml-user-select: none;
+                -moz-user-select: none;
+                -ms-user-select: none;
+                user-select: none;
+            }
+        `;
+        const head = document.head || document.getElementsByTagName('head')[0];
+        const style = document.createElement('style');
+        style.appendChild(document.createTextNode(css));
+        head.appendChild(style);
+    },[]);
+
+    const value = null;
+
+    return(
+        <DesktopEnvironmentContext.Provider value={value}>
+            {children}
+        </DesktopEnvironmentContext.Provider>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+import WindowManagerContextProvider from 'react-window-manager';
+
+ReactDOM.render(
+  <WindowManagerContextProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </WindowManagerContextProvider>,
+  document.getElementById('root')
+);
+
